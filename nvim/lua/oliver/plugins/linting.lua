9e526b7fbf670723ef75d0f275f066f6dc148ce8
oliver.kutis <analytika.oliver.kutis@gmail.com>
2025-04-15 21:15:50 +0200

    changed keymap

--- nvim/lua/oliver/plugins/linting.lua
+++ nvim/lua/oliver/plugins/linting.lua
@@ -5,7 +5,7 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			json = { "jsonlint" },
+			-- json = { "jsonlint" },
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
@@ -28,7 +28,7 @@ return {
 			end,
 		})
 
-		vim.keymap.set("n", "<leader>l", function()
+		vim.keymap.set("n", "<leader>ll", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
 	end,
