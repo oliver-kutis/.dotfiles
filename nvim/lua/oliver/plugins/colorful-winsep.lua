85d6aa4c8de69ffa15237dd90b07b1f7abd7d6b4
oliver.kutis <analytika.oliver.kutis@gmail.com>
2025-04-16 17:37:13 +0200

    plugin for colorful separated windows

--- nvim/lua/oliver/plugins/colorful-winsep.lua
+++ nvim/lua/oliver/plugins/colorful-winsep.lua
@@ -1,5 +1,13 @@
 return {
 	"nvim-zh/colorful-winsep.nvim",
-	config = true,
+	-- config = true,
 	event = { "WinLeave" },
+	config = function()
+		require("colorful-winsep").setup({
+			hi = {
+				bg = "#16161E",
+				fg = "#89b4fa",
+			},
+		})
+	end,
 }
